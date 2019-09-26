f1cf2c274d361dcf6602ca32d9b721b710bfec5b
tacaro <tristan.caro@colorado.edu>
2019-09-26 12:47:20 -0600

    implemented read_stdin_col as a module

--- get_data.py
+++ get_data.py
@@ -1,3 +1,19 @@
+import sys
+import os
+import math
+
+
 
 def read_stdin_col(col_num):
-    return None
+    A = []
+    V = []
+    for l in sys.stdin:
+        A = [x for x in l.split()]
+        V.append(int(A[col_num]))
+    return V
+
+#def main():
+    #print(read_stdin_col(0))
+
+#if __name__ == '__main__':
+    #main()
